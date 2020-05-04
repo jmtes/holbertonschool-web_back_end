7ec1fe7a81d629c747bb25ada271f96004d2015f
jmtes <juliennetesoro@gmail.com>
2020-05-04 09:33:27 -0700

    Add script

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+''' Define add function.
+
+    Attributes:
+      add - function that performs addition of two floats
+'''
+
+
+def add(a: float, b: float) -> float:
+    ''' Return sum of a and b. '''
+    return a + b
