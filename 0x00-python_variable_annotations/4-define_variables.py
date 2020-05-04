6e4e98003bcb7a543e224e2a91525d9920998178
jmtes <juliennetesoro@gmail.com>
2020-05-04 12:39:31 -0700

    Adjust type annotations

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -9,7 +9,7 @@
 '''
 
 
-a = 1  # type: int
-pi = 3.14  # type: float
-i_understand_annotations = True  # type: bool
-school = 'Holberton'  # type: str
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
