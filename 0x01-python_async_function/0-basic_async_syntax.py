398615ef7eadc75208c09c8ee24fb6922a79d15e
jmtes <juliennetesoro@gmail.com>
2020-05-05 13:57:43 -0700

    Import entire random module

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,11 +2,11 @@
 ''' Define wait_random function. '''
 
 from asyncio import sleep
-from random import random
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
     ''' Wait up to `max_delay` seconds and then return length of delay. '''
-    delay = max_delay * random()
+    delay = max_delay * random.random()
     await sleep(delay)
     return delay
