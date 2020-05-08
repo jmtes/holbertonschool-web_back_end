1ba6a81b94f49624a7d9d7149a273147334bdfb9
jmtes <juliennetesoro@gmail.com>
2020-05-08 13:16:52 -0700

    Define LFU cache

---
+++ 0x03-caching/100-lfu_cache.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+''' Define LFUCache class. '''
+
+BaseCaching = __import__('base_cache').BaseCaching
+
+
+class LFUCache(BaseCaching):
+    ''' An LFU cache.
+
+        Inherits all behaviors from BaseCaching except, upon any attempt to add
+        an entry to the cache when it is at max capacity (as specified by
+        BaseCaching.MAX_ITEMS), it discards the least frequently used entry to
+        accommodate for the new one.
+
+        Attributes:
+          __init__ - method that initializes class instance
+          put - method that adds a key/value pair to cache
+          get - method that retrieves a key/value pair from cache '''
+
+    def __init__(self):
+        ''' Initialize class instance. '''
+        super().__init__()
+        self.keys = []
+        self.uses = {}
+
+    def put(self, key, item):
+        ''' Add key/value pair to cache data.
+
+            If cache is at max capacity (specified by BaseCaching.MAX_ITEMS),
+            discard least frequently used entry to accommodate new entry. '''
+
+        # if len(self.cache_data) > BaseCaching.MAX_ITEMS and key not in
+        # self.cache_data:
+
+        # self.cache_data[key] = item
+        # self.uses[key] = self.uses[key] + 1 if key in self.uses else 0
+
+        if key is not None and item is not None:
+            if len(self.keys) == BaseCaching.MAX_ITEMS and key not in self.keys:
+                discard = self.keys.pop(self.keys.index(self.findLFU()))
+                del self.cache_data[discard]
+                del self.uses[discard]
+                print('DISCARD: {:s}'.format(discard))
+            self.cache_data[key] = item
+            if key not in self.keys:
+                self.keys.append(key)
+                self.uses[key] = 0
+            else:
+                self.keys.append(self.keys.pop(self.keys.index(key)))
+                self.uses[key] += 1
+
+    def get(self, key):
+        ''' Return value stored in `key` key of cache.
+            If key is None or does not exist in cache, return None. '''
+        if key is not None and key in self.cache_data:
+            self.keys.append(self.keys.pop(self.keys.index(key)))
+            self.uses[key] += 1
+            return self.cache_data[key]
+        return None
+
+    def findLFU(self):
+        items = list(self.uses.items())
+        freqs = [item[1] for item in items]
+        least = min(freqs)
+
+        # print('Inside findLFU')
+        # print('items is', items)
+        # print('freqs is', freqs)
+        # print('least is', least)
+
+        lfus = [item[0] for item in items if item[1] == least]
+        for key in self.keys:
+            if key in lfus:
+                return key
