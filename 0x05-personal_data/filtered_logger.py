6b25d55a80ce9ba5519c1a0630b1e1ed00232ba7
jmtes <juliennetesoro@gmail.com>
2020-05-16 22:09:20 -0700

    Implement format method

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -29,10 +29,17 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
+    def __init__(self, fields: List[str]):
         ''' Initialize class instance. '''
         super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        ''' Filter values in log records. '''
-        raise NotImplementedError
+        ''' Return formatted log record string with PII redacted. '''
+        return filter_datum(
+            self.fields,
+            self.REDACTION,
+            super(
+                RedactingFormatter,
+                self).format(record),
+            self.SEPARATOR)
