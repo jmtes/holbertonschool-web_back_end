9b8c5d7651c44c958566efa7e91dcbfdabf6dc42
jmtes <juliennetesoro@gmail.com>
2020-05-18 00:05:54 -0700

    Implement password validation

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -8,3 +8,9 @@ def hash_password(password: str) -> bytes:
     ''' Return encrypted password. '''
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+  ''' Check whether or not password matches hashed password. '''
+  if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+    return True
+  return False
