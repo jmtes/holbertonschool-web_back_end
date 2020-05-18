57e50a00b60f533f4643704fc8791d3c914cd751
jmtes <juliennetesoro@gmail.com>
2020-05-17 17:24:59 -0700

    Edit regular expression

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -12,7 +12,7 @@ PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'ip')
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     ''' Return `message` with personally identifiable information redacted. '''
-    regex = r'(\w+)=([\w\-.]+@*[\w\-.]+)'
+    regex = r'(\w+)=([\w\-./]+@*[\w\-./]+)'
     return sub(regex, lambda x: x.group(1) + '=' + redaction
                if x.group(1) in fields else x.group(), message)
 
