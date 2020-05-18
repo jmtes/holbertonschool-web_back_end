b9106724639322b64da117c86b9f3451c148ffdf
jmtes <juliennetesoro@gmail.com>
2020-05-18 00:06:10 -0700

    Make pep8 compliant

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -9,8 +9,9 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode('utf-8'), salt)
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
-  ''' Check whether or not password matches hashed password. '''
-  if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
-    return True
-  return False
+    ''' Check whether or not password matches hashed password. '''
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        return True
+    return False
