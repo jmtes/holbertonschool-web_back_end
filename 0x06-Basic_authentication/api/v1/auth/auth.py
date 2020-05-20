248c83176f0aa228942f03f03752ade21c81609b
jmtes <juliennetesoro@gmail.com>
2020-05-19 17:43:42 -0700

    Implement authorization_header function

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -6,6 +6,13 @@ from typing import List, TypeVar
 
 
 class Auth:
+    ''' Auth Controller
+
+        Methods:
+          require_auth - Check if path requires authentication
+          authorization_header - Check if request is authorized
+          current_user - '''
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         ''' Determine whether or not path requires authentication.
 
@@ -23,7 +30,10 @@ class Auth:
         return False if path in excluded_paths else True
 
     def authorization_header(self, request=None) -> str:
-        return None
+        ''' Return value of Authorization header if request is authorized, False otherwise. '''
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         return None
