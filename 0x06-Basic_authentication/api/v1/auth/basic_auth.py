76b1adda8fa1ed9411cc4c6e140b80eab915d989
jmtes <juliennetesoro@gmail.com>
2020-05-20 12:25:51 -0700

    Implement class method

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,11 @@ from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) \
+            -> str:
+        ''' Return authorization header if valid. '''
+        if authorization_header is None or not isinstance(authorization_header,
+                                                          str):
+            return None
+        return authorization_header[6:] \
+            if authorization_header.startswith('Basic ') else None
