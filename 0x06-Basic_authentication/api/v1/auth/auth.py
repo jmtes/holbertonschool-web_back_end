efb89e3b9766373067163c9aa94290efbc4cce1d
jmtes <juliennetesoro@gmail.com>
2020-05-21 11:57:01 -0700

    Make pep8 compliant

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -30,10 +30,12 @@ class Auth:
         return False if path in excluded_paths else True
 
     def authorization_header(self, request=None) -> str:
-        ''' Return value of Authorization header if request is authorized, False otherwise. '''
+        ''' Return value of Authorization header if request is authorized,
+        False otherwise. '''
         if request is None or 'Authorization' not in request.headers:
             return None
         return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
+        ''' Return current user. '''
         return None
