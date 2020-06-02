b23760dcb660261f12775d6a739919406959c2e0
jmtes <juliennetesoro@gmail.com>
2020-06-02 02:33:35 +0000

    Remove dependency

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -6,7 +6,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
-import bcrypt
 
 from user import Base, User
 
