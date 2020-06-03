efd6e84290fffc536df371012171b5fba30b3897
jmtes <juliennetesoro@gmail.com>
2020-06-03 19:22:50 +0000

    Implement user search by session ID

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 from uuid import uuid4
+from typing import Union
 
 
 def _hash_password(password: str) -> str:
@@ -53,5 +54,16 @@ class Auth:
             self._db.update_user(user.id, session_id=session_id)
 
             return session_id
+        except NoResultFound:
+            return None
+
+    def get_user_from_session_id(self, session_id: str) -> Union[None, str]:
+        ''' Return user associated with session ID. '''
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user.email
         except NoResultFound:
             return None
