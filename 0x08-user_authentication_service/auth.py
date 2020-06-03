2b601b24768d4094ed91d15fdf1cff3211b5c2a4
jmtes <juliennetesoro@gmail.com>
2020-06-03 18:01:32 +0000

    Implement password checking

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -30,3 +30,11 @@ class Auth:
             registered_user = self._db.add_user(email,
                                                 _hash_password(password))
             return registered_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        ''' Check if password matches. '''
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
