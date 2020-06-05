5819b44bd04e9d06598523940a0c3f052ef2e000
jmtes <juliennetesoro@gmail.com>
2020-06-05 11:34:05 -0700

    Add tests

---
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+''' Testing suite for utils.py. '''
+
+from unittest import TestCase
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(TestCase):
+    ''' Test utils.access_nested_map '''
+
+    @parameterized.expand([
+        ({'a': 1}, ('a',), 1),
+        ({'a': {'b': 2}}, ('a',), {'b': 2}),
+        ({'a': {'b': 2}}, ('a', 'b'), 2)
+    ])
+    def test_access_nested_map(self, input_map, input_path, expected_output):
+        ''' Test utils.access_nested_map '''
+        actual = access_nested_map(input_map, input_path)
+        assert actual == expected_output
