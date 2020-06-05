e8f65f82a8a2acf19a5f108c3e0f4a62289dc63c
jmtes <juliennetesoro@gmail.com>
2020-06-05 11:48:30 -0700

    Add exception tests and change variable names

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -14,7 +14,18 @@ class TestAccessNestedMap(TestCase):
         ({'a': {'b': 2}}, ('a',), {'b': 2}),
         ({'a': {'b': 2}}, ('a', 'b'), 2)
     ])
-    def test_access_nested_map(self, input_map, input_path, expected_output):
+    def test_access_nested_map(self, map, path, expected):
         ''' Test utils.access_nested_map '''
-        actual = access_nested_map(input_map, input_path)
-        self.assertEqual(actual, expected_output)
+        actual = access_nested_map(map, path)
+        self.assertEqual(actual, expected)
+
+    @parameterized.expand([
+        ({}, ('a',), 'a'),
+        ({'a': 1}, ('a', 'b'), 'b')
+    ])
+    def test_access_nested_map_exception(
+            self, map, path, expected):
+        ''' Test utils.access_nested_map exceptions. '''
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(map, path)
+            self.assertEqual(expected, str(e.exception))
