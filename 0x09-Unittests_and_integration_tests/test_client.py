6b7820fa0251b67a0eb2dbda7b51e0603ff4621e
jmtes <juliennetesoro@gmail.com>
2020-06-09 13:28:28 -0700

    Add test

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -4,6 +4,7 @@
 from unittest import TestCase, mock
 from parameterized import parameterized
 import client
+from client import GithubOrgClient
 
 
 class TestGithubOrgClient(TestCase):
@@ -23,3 +24,13 @@ class TestGithubOrgClient(TestCase):
         goc.org()
         mock_get_json.assert_called_once_with(
             'https://api.github.com/orgs/{}'.format(org_name))
+
+    def test_public_repos_url(self):
+        ''' Test GithubOrgClient.public_repos_url property. '''
+
+        with mock.patch('client.GithubOrgClient._public_repos_url', new_callable=mock.PropertyMock) as m:
+            m.return_value = 'test'
+
+            goc = GithubOrgClient('buttercup')
+            pru = goc._public_repos_url
+            self.assertEqual(pru, 'test')
