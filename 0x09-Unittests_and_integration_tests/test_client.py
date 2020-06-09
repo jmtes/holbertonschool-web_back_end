333c901d5ab12e4912235dfaa3ae6fd3e5c9915e
jmtes <juliennetesoro@gmail.com>
2020-06-09 12:53:58 -0700

    Use patch decorator

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -14,12 +14,12 @@ class TestGithubOrgClient(TestCase):
         ('verne'),
         ('koi')
     ])
-    def test_org(self, org_name):
+    @mock.patch('client.get_json')
+    def test_org(self, org_name, mock_get_json):
         ''' Test GithubOrgClient.org() '''
 
         goc = client.GithubOrgClient(org_name)
 
-        with mock.patch('client.get_json') as m:
-            goc.org()
-            m.assert_called_once_with(
-                'https://api.github.com/orgs/{}'.format(org_name))
+        goc.org()
+        mock_get_json.assert_called_once_with(
+            'https://api.github.com/orgs/{}'.format(org_name))
