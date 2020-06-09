b82d9c38520e93c940bda6031af5209d2199592b
jmtes <juliennetesoro@gmail.com>
2020-06-09 12:49:28 -0700

    Parameterize test

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -2,17 +2,24 @@
 ''' Test client.py '''
 
 from unittest import TestCase, mock
+from parameterized import parameterized
 import client
 
 
 class TestGithubOrgClient(TestCase):
     ''' Test class GithubOrgClient '''
 
-    def test_org(self):
+    @parameterized.expand([
+        ('buttercup'),
+        ('verne'),
+        ('koi')
+    ])
+    def test_org(self, org_name):
         ''' Test GithubOrgClient.org() '''
 
-        goc = client.GithubOrgClient('buttercup')
+        goc = client.GithubOrgClient(org_name)
 
         with mock.patch('client.get_json') as m:
             goc.org()
-            m.assert_called_once_with('https://api.github.com/orgs/buttercup')
+            m.assert_called_once_with(
+                'https://api.github.com/orgs/{}'.format(org_name))
