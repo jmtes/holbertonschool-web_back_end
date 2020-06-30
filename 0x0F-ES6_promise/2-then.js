03f096d105ab4d5232eff5adac1f87ca25514d25
jmtes <juliennetesoro@gmail.com>
2020-06-30 13:07:53 -0700

    Tack then, catch, and finally to return promise

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,14 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({
+  return promise.then(() => ({
     status: 200,
     body: 'success',
-  }));
-
-  promise.catch(() => new Error());
-
-  promise.finally(() => {
+  })).catch(() => new Error()).finally(() => {
     console.log('Got a response from the API');
   });
-
-  return promise;
 }
