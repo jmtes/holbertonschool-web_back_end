2ac79083e93bb9b9fce1705f9aa9e960dbf562a3
jmtes <juliennetesoro@gmail.com>
2020-07-01 18:06:42 -0700

    Define EVCar class

---
+++ 0x10-ES6_classes/100-evcar.js
@@ -0,0 +1,18 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    if (typeof range !== 'string') throw TypeError('range must be a string');
+
+    this._range = range;
+  }
+
+  cloneCar() {
+    const clone = new (Object.getPrototypeOf(this.constructor))(this._brand,
+      this._motor, this._color);
+    clone._range = this._range;
+
+    return clone;
+  }
+}
