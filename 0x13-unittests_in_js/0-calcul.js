7faf27215651d20000042eedbd1fb5448ec8ec1d
jmtes <juliennetesoro@gmail.com>
2020-07-15 17:43:35 -0700

    Return sum of cast numbers

--- 0x13-unittests_in_js/0-calcul.js
+++ 0x13-unittests_in_js/0-calcul.js
@@ -5,5 +5,5 @@ module.exports = function calculateNumber(a, b = 0) {
   if (Number.isNaN(aNum) || Number.isNaN(bNum))
     throw TypeError('Parameters must be numbers or able to coerce to number');
 
-  return Math.round(a) + Math.round(b);
+  return Math.round(aNum) + Math.round(bNum);
 };
