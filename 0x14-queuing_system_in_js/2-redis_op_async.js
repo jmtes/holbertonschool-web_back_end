0afc97e4fb0a82042fc0143058f5e97289880a81
jmtes <juliennetesoro@gmail.com>
2020-07-20 15:23:39 -0700

    Promisify value retrieval from store

---
+++ 0x14-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,27 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+
+const get = promisify(client.get).bind(client);
+
+client.on('connect', () => console.log('Redis client connected to the server'));
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+  const val = await get(schoolName);
+  console.log(val);
+}
+
+(async function main() {
+  await displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+}());
